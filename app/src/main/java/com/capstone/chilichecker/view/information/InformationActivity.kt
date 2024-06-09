71578d4bf9e6b7ce8b158cf0cb3ee206db072657
Ryan Rizky Ramadhan <ryanrizky14@gmail.com>
2024-06-10 01:06:53 +0700

    Update Implement All Layout

---
+++ app/src/main/java/com/capstone/chilichecker/view/information/InformationActivity.kt
@@ -0,0 +1,19 @@
+package com.capstone.chilichecker.view.information
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.capstone.chilichecker.R
+import com.capstone.chilichecker.databinding.ActivityInformationBinding
+
+class InformationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInformationBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInformationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
--- app/src/main/java/com/capstone/chilichecker/view/main/MainActivity.kt
+++ app/src/main/java/com/capstone/chilichecker/view/main/MainActivity.kt
@@ -1,38 +1,76 @@
 package com.capstone.chilichecker.view.main
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.capstone.chilichecker.R
 import com.capstone.chilichecker.databinding.ActivityMainBinding
+import com.capstone.chilichecker.view.bookmark.BookmarkActivity
+import com.capstone.chilichecker.view.information.InformationActivity
+import com.capstone.chilichecker.view.maps.MapsActivity
+import com.capstone.chilichecker.view.scan.ScanActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        supportActionBar?.hide()
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        supportActionBar?.hide()
+        navigation()
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_bookmark, R.id.navigation_recommendation
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_app_bar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_setting -> {
+                //Coming Soon
+                true
+            }
+
+            R.id.menu_logout -> {
+                //Coming Soon
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun navigation() {
+        binding.btnScanChili.setOnClickListener {
+            val intent = Intent(this, ScanActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.btnBookmark.setOnClickListener {
+            val intent = Intent(this, BookmarkActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.btnGeoChili.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.btnInformation.setOnClickListener {
+            val intent = Intent(this, InformationActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
